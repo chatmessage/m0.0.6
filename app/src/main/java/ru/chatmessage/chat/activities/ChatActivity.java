d3638420bba5cd24161a910724faf06808e2610f
Sevagog <seva75699@mail.ru>
2022-05-26 18:22:02 +0300

    add firends fragment

--- app/src/main/java/ru/chatmessage/chat/activities/ChatActivity.java
+++ app/src/main/java/ru/chatmessage/chat/activities/ChatActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import ru.chatmessage.chat.R;
 
@@ -37,6 +40,18 @@ public class ChatActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.container, chatFragment, "chatFragment");
         transaction.commit();
+
+        ImageButton bexit = (ImageButton) findViewById(R.id.bexit);
+
+        bexit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FriendFragment friendFragment = new FriendFragment();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.container, friendFragment, "friendFragment");
+                transaction.commit();
+            }
+        });
+
     }
 
 }
